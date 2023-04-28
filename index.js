8dbae95ccfe8f68567eb8c14c46b804c8d1ffb28
AlexaDickenson <127786567+AlexaDickenson@users.noreply.github.com>
2023-04-27 21:35:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Variables
+const name = "John";
+let age = 30;
+
+// Functions
+function greet() {
+  console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+}
+
+function double(num) {
+  return num * 2;
+}
+
+function add(a, b) {
+  return a + b;
+}
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Variables
+const myName: string = "John";
+let age: number = 30;
+
+// Functions
+function greet(): void {
+  console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+}
+
+function double(num: number): number {
+  return num * 2;
+}
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
